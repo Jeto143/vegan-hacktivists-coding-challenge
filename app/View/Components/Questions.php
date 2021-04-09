3f0f6c82816578473d0d3d5d7f474ccab9ff1cfd
Jérémy Touati <jeremy.touati@gmail.com>
2021-04-09 13:19:32 +0200

    Add PHPDoc for `$questions`'s type

--- app/View/Components/Questions.php
+++ app/View/Components/Questions.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 namespace App\View\Components;
 
+use App\Models\Question;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 final class Questions extends Component
 {
+    /** @var iterable|Question[]  */
     public iterable $questions;
 
+    /**
+     * @param iterable|Question[] $questions
+     */
     public function __construct(iterable $questions)
     {
         $this->questions = $questions;
