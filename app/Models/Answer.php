5ae0ce023a6ee14a02ad846e34071767cdbd726a
Jérémy Touati <jeremy.touati@gmail.com>
2021-04-09 14:04:21 +0200

    Add answer -> question relationship

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -6,8 +6,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 final class Answer extends Model
 {
     use HasFactory;
+
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
