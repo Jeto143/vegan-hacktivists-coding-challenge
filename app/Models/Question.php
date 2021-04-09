1f55ee49390172262c8524b7e19d7d7030296d6d
Jérémy Touati <jeremy.touati@gmail.com>
2021-04-09 12:55:39 +0200

    Add question -> answers relationship

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,8 +6,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 final class Question extends Model
 {
     use HasFactory;
+
+    public function answers(): HasMany
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
