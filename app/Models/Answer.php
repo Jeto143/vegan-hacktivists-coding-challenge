f6772a8415678bdfc20803e6954fc97525ceac36
Jérémy Touati <jeremy.touati@gmail.com>
2021-04-09 14:05:12 +0200

    Add PHPDoc for models' magic properties

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property string $text
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property Question $question
+ */
 final class Answer extends Model
 {
     use HasFactory;
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property Answer[] $answers
+ */
 final class Question extends Model
 {
     use HasFactory;
