1b1bdf8a2e430a505943361982ceba37bde11bca
Jérémy Touati <jeremy.touati@gmail.com>
2021-04-09 14:07:05 +0200

    Add `idSlug` attribute for cooler URLs

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,6 +8,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 /**
  * @property int $id
@@ -23,5 +24,14 @@ final class Question extends Model
     public function answers(): HasMany
     {
         return $this->hasMany(Answer::class);
+    }
+
+    /**
+     * Retrieves the ID of this question concatenated with a slug of its title.
+     * This can appear in URLs and be converted back to int to get the numeric ID.
+     */
+    public function getIdSlugAttribute(): string
+    {
+        return $this->id . '_' . Str::slug($this->title);
     }
 }
